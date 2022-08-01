1399ccc4dade6edfb9ef1c364d42ba29c54ca46c
Quentin Caritey <quentin.caritey@neoxia.com>
2022-08-01 17:29:37 +0200

    update names

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-medecin'),
+      dir: require('path').join(__dirname, './coverage/veterinaire-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'https://docto-congo-back.herokuapp.com',
-  p2p: 'https://docto-congo-p2p-www.herokuapp.com',
+  api: 'https://veto-lib-back.herokuapp.com',
+  p2p: 'https://veto-lib-p2p-www.herokuapp.com',
   auth: {
     tenantId: 'eae410ed-7a7e-451a-b931-636bbc191a74',
     clientId: '40cb24bc-81b4-44d7-9f96-2e2a089ce685',
