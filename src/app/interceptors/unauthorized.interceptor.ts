7dc7ad277dec63e4d0d6b645e991d139f09f4a98
Quentin Caritey <quentin.caritey@neoxia.com>
2022-08-09 17:04:38 +0200

    fix register user flow

--- src/app/interceptors/unauthorized.interceptor.ts
+++ src/app/interceptors/unauthorized.interceptor.ts
@@ -3,29 +3,32 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+
+import { Observable, of, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class UnauthorizedInterceptor implements HttpInterceptor {
-
   constructor(private auth: AuthService, private router: Router) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      catchError(err => {
+      catchError((err) => {
         if (err.status === 401) {
           this.auth.login();
           this.router.navigate(['']);
+          return of();
         }
-        return of(err);
+        return throwError(() => err);
       })
     );
   }
-
 }
