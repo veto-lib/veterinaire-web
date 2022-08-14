ef1f80e8a1f9563c0a0d6e929f1adb141e1f0bdf
Quentin Caritey <quentin.caritey@neoxia.com>
2022-08-14 12:34:50 +0200

    customer treeview revamp

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,39 +1,63 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit, Component } from '@angular/core';
+
+import { FlatTreeControl } from '@angular/cdk/tree';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
 
 import { CustomersService } from 'src/app/services/customers.service';
 import { AuthService } from 'src/app/services/auth.service';
 
 import { ICustomer } from 'src/app/models/customer';
+import { IAnimal } from 'src/app/models/animal';
+
+interface FlatNode {
+  expandable: boolean;
+  value: ICustomer | IAnimal;
+  level: number;
+}
 
 @Component({
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.less'],
 })
 export class CustomersComponent implements AfterViewInit {
+  private _transformer = (
+    node: ICustomer | IAnimal,
+    level: number
+  ): FlatNode => {
+    return {
+      expandable:
+        node.kind === 'customer' && !!node.animals && node.animals.length > 0,
+      value: node,
+      level: level,
+    };
+  };
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  dataSource: MatTableDataSource<ICustomer> = new MatTableDataSource([] as ICustomer[]);
+  treeControl = new FlatTreeControl<FlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
 
-  displayedColumns: string[] = [
-    'firstName',
-    'lastName',
-    'birthDate',
-    'gender',
-    'medicalRecord'
-  ];
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => (node.kind === 'customer' ? node.animals : [])
+  );
 
-  constructor(private service: CustomersService, private auth: AuthService) {}
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor(private service: CustomersService, private auth: AuthService) {
+    this.dataSource.data = [];
+  }
 
   ngAfterViewInit(): void {
-    this.service.getClinicCustomers().subscribe(customers => {
-      this.dataSource = new MatTableDataSource(customers);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+    this.service.getClinicCustomers().subscribe((customers) => {
+      this.dataSource.data = customers;
     });
   }
 
+  hasChild = (_: number, node: FlatNode) => node.expandable;
 }
