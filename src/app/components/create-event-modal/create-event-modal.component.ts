2b4e9d951fdaef943d0781d1486e1a7a245215cf
Quentin Caritey <quentin.caritey@neoxia.com>
2022-08-15 16:49:59 +0200

    create event modal fix

--- src/app/components/create-event-modal/create-event-modal.component.ts
+++ src/app/components/create-event-modal/create-event-modal.component.ts
@@ -10,10 +10,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import * as moment from 'moment';
 
 import { ICustomer } from 'src/app/models/customer';
+import { IAnimal } from 'src/app/models/animal';
 import { CreateEvent } from 'src/app/models/event';
+import { EventReason } from 'src/app/models/common';
 
 import { CustomersService } from 'src/app/services/customers.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { AnimalsService } from 'src/app/services/animals.service';
 
 @Component({
   templateUrl: './create-event-modal.component.html',
@@ -24,10 +27,19 @@ export class CreateEventModalComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public date: Date,
     private fb: FormBuilder,
     private customersService: CustomersService,
+    private animalsService: AnimalsService,
     private auth: AuthService
   ) {}
 
   customers: ICustomer[] = [];
+  animals: IAnimal[] = [];
+
+  eventReasons: EventReason[] = [
+    'Vaccination',
+    'Consultation',
+    'Identification',
+    'Visite de contrôle',
+  ];
 
   form = this.fb.group(
     {
@@ -35,6 +47,8 @@ export class CreateEventModalComponent implements OnInit {
       start: ['', Validators.required],
       end: ['', Validators.required],
       customer: ['', Validators.required],
+      animal: ['', Validators.required],
+      motive: ['', Validators.required],
       notes: ['', Validators.required],
     },
     {
@@ -46,10 +60,18 @@ export class CreateEventModalComponent implements OnInit {
     this.customersService
       .getClinicCustomers()
       .subscribe((customers) => (this.customers = customers));
-    this.form.controls['start'].setValue(this.date.toDateString());
+    this.form.controls['start'].setValue(this.date.toISOString());
     this.form.controls['end'].setValue(
-      moment(this.date).add(1, 'h').toDate().toDateString()
+      moment(this.date).add(1, 'h').toDate().toISOString()
     );
+    this.form.controls['customer'].valueChanges.subscribe((customerEmail) => {
+      this.animals = [];
+      this.animalsService
+        .getCustomerAnimals(customerEmail as string)
+        .subscribe((animals) => {
+          this.animals = animals;
+        });
+    });
   }
 
   endDateAfterStartDate(group: AbstractControl): ValidationErrors | null {
@@ -68,8 +90,8 @@ export class CreateEventModalComponent implements OnInit {
       customer: this.form.value.customer as string,
       notes: this.form.value.notes as string,
       veterinary: this.auth.email,
-      animal: '1',
-      reason: 'Consultation',
+      animal: (this.form.value.animal as string).toString(),
+      reason: this.form.value.motive as EventReason,
     };
   }
 }
